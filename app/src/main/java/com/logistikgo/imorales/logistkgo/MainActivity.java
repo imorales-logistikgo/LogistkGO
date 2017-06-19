550e8d25a13f062be336f494892c847e1317f7e0
imorales-logistikgo <imorales@logistikgo.com>
2017-06-19 14:10:42 -0500

    Initial commit

---
+++ app/src/main/java/com/logistikgo/imorales/logistkgo/MainActivity.java
@@ -0,0 +1,20 @@
+package com.logistikgo.imorales.logistkgo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void buscarPelicula(View view) {
+        Toast.makeText(this, "mensaje", Toast.LENGTH_LONG).show();
+    }
+
+}
